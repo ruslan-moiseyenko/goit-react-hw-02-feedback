278c4281f8dae1f9f5e7a2f284ea8f58f121ed2c
Ruslan Moiseyenko <ruscom5@gmail.com>
2022-03-18 22:17:26 +0100

    finished all the tasks. statistics shoes only when some feedbacks given

--- src/components/feedback/FeedbackOptions.jsx
+++ src/components/feedback/FeedbackOptions.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import {Button} from './Feedback.styled';
+
 const FeedbackOptions = ({ onGood, onNeutral, onBad }) => {
   return (
     <>
-      <button type="button" onClick={onGood}>
+      <Button type="button" onClick={onGood}>
         Good
-      </button>
-      <button type="button" onClick={onNeutral}>
+      </Button>
+      <Button type="button" onClick={onNeutral}>
         Neutral
-      </button>
-      <button type="button" onClick={onBad}>
+      </Button>
+      <Button type="button" onClick={onBad}>
         Bad
-      </button>
+      </Button>
     </>
   );
 };
--- src/components/feedback/Statistics.jsx
+++ src/components/feedback/Statistics.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage = 10 }) => {
-  console.log(positivePercentage);
   return (
     <>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>Total: {total}</p>
-      <p>PositivePercentage: { Math.round(positivePercentage) || 0}%</p>
+      {!total ? (
+        <p>No feedback given</p>
+      ) : (
+        <>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>Total: {total}</p>
+          <p>PositivePercentage: {Math.round(positivePercentage) || 0}%</p>
+        </>
+      )}
     </>
   );
 };
